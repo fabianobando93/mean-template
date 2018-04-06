d1ac76c355d9e1cfa8312bb1abe33877e8205688
Fabian Mauricio Obando Diaz <fobando@cafetosoftware.local>
2018-04-05 20:17:05 -0500

    Created app-ui

--- app-server/models/item.js
+++ app-server/models/item.js
@@ -14,3 +14,23 @@ const ItemSchema = mongoose.Schema({
 
 //Create a model using mongoose.model and export it
 const Items = module.exports = mongoose.model('Items', ItemSchema );
+
+module.exports.getAll = (callback) => {
+    Items.find(callback);
+}
+
+module.exports.getItemById = (id, callback) => {
+    let query = {_id: id};
+    Items.find(query, callback);
+}
+
+//newItem.save is used to insert the document into MongoDB
+module.exports.addItem = (newList, callback) => {
+    Items.save(callback);
+}
+
+//Here we need to pass an id parameter to BUcketList.remove
+module.exports.deleteItemById = (id, callback) => {
+    let query = {_id: id};
+    Items.remove(query, callback);
+}
--- app-server/services/item-service.js
+++ app-server/services/item-service.js
@@ -2,5 +2,51 @@ const express = require('express');
 const router = express.Router();
 const items = require('../models/item');
 
+router.get('/',(req,res) => {
+    items.getAll((err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+});
+
+router.post('/', (req,res,next) => {
+    let newItem = new item({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category
+    });
+    items.addItem(newItem,(err, list) => {
+        if(err) {
+            res.json({success: false, message: `Failed to create a new list. Error: ${err}`});
+
+        }
+        else
+            res.json({success:true, message: "Added successfully."});
+
+    });
+});
+
+router.delete('/:id', (req,res,next)=> {
+
+      let id = req.params.id;
+
+      item.deleteItemById(id,(err,list) => {
+          if(err) {
+              res.json({success:false, message: `Failed to delete the list. Error: ${err}`});
+          }
+          else if(list) {
+              res.json({success:true, message: "Deleted successfully"});
+          }
+          else
+              res.json({success:false});
+      })
+  });
 
 module.exports = router;
